d3e5ccc489534503958445cf57e5244945196647
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2021-02-14 05:20:51 +0900

    userのentriesを取得ｓする関数を作成

--- src/util/bookmark.py
+++ src/util/bookmark.py
@@ -81,6 +81,10 @@ class Bookmark:
             return []
         max_page = self.count_bookmark_page(hatena_id, option)
 
+        if max_page > 10:
+            # 最大200件まで取得するようにする
+            max_page = 10
+
         links = []
 
         for i in range(max_page):
@@ -122,3 +126,45 @@ class Bookmark:
             entries.append(
                 dict(link=entry['link'], title=entry['title'], recommendation_score=osusume))
         return entries
+
+    def get_user_entries(self, hatena_id: str, option: str = '') -> list[feedparser.util.FeedParserDict]:
+        # 1ページに20件のデータがある。ページ数を求める
+        if hatena_id == "":
+            return []
+        max_page = self.count_bookmark_page(hatena_id, option)
+
+        if max_page > 10:
+            # 最大200件まで取得するようにする
+            max_page = 10
+        entries = []
+        for i in range(max_page):
+            data = requests.get(
+                'https://b.hatena.ne.jp/{}/bookmark.rss?{}page={}'.format(hatena_id, option, i+1))
+            d = feedparser.parse(data.text)
+            entries += d['entries']
+        return entries
+
+    def get_suggestion(self, hatena_id: str, option: str) -> list[dict[str, str]]:
+        if hatena_id == "":
+            return ""
+        dic = {}
+        data = []
+
+        entries = self.get_user_entries(hatena_id, option)
+
+        w = word.find_word(hatena_id)
+        if len(w) == 0:
+            # 検索結果が0だったら何もしない
+            print("no data")
+            return [dict(link="", title="タイトル", score="データがありません")]
+        for entry in entries:
+            noun = wd.get_noun(entry['title'])
+            dic.setdefault(entry['link'], 0)
+            for n in noun:
+                if n in w:
+                    dic[entry['link']] += w[n]
+
+            data.append(
+                dict(link=entry['link'], title=entry['title'], score=dic[entry['link']]))
+
+        return data
